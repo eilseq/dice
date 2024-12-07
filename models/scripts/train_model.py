c4255d05a08f70d916a4368f38171a58404ef2fe
eilseq <tools@eilseq.com>
2024-12-07 17:05:00 +0100

    improve training with validation and testing

--- models/scripts/train_model.py
+++ models/scripts/train_model.py
@@ -4,149 +4,169 @@ import os
 import torch
 import torch.optim as optim
 
-from dice_datasets import RandomPatternConfig
-from dice_models import createDiceModel, DiceArchitecture, createDiceLoss, DiceLoss
+from dice_datasets import Pattern, RandomPatternConfig
+from dice_models import createDiceModel, createDiceLoss
 from rich.progress import Progress, Live, BarColumn, TextColumn, TimeElapsedColumn, TaskProgressColumn
-from torch.utils.data import DataLoader
-
+from rich.console import Console
+from rich.text import Text
+from torch.utils.data import DataLoader, random_split
 
 # Enable MacOSX GPU Acceleration
 device = torch.device('mps')
 
+console = Console()
+progress = Progress(
+    "[progress.description]{task.description}",
+    TextColumn(
+        "[green]Entries: {task.fields[entries_used]}/{task.fields[total_entries]}"),
+    TextColumn("[red]Loss: {task.fields[loss]:.4f}"),
+    TextColumn("[purple]Validation Loss: {task.fields[validation_loss]:.4f}"),
+    TimeElapsedColumn(),
+)
+
 
 def parse_args():
-    parser = argparse.ArgumentParser(description="Process some inputs.")
-
+    parser = argparse.ArgumentParser(description="Train a DICE model")
     parser.add_argument('--id', type=str, required=True,
                         help='Model identifier')
-
-    parser.add_argument('--preset', type=str, required=True,
-                        help='Selected pattern preset')
-
-    parser.add_argument('--architecture', type=str, required=True,
-                        help='Selected model architecture:\n conv_auto_enc - Convolutional Autoencoder \n att_unet - Attention UNet')
-
+    parser.add_argument('--preset', type=str,
+                        required=True, help='Pattern preset')
+    parser.add_argument('--architecture', type=str,
+                        required=True, help='Model architecture')
     parser.add_argument('--loss', type=str, required=True,
-                        help='Selected loss function:\n mse_poly_penalty - Mean Square Error With Pattern Validation Penalty \n l1_poly_penalty - L1 Error With Pattern Validation Penalty')
-
-    parser.add_argument('--noise_level', type=float, required=True,
-                        help='Noise level used during training of diffusion model, usually between 0.1 and 0.2')
-
-    parser.add_argument('--batch_size', type=int, default=32,
-                        help='Batch size used in model training')
-
-    parser.add_argument('--epochs', type=int, default=20,
-                        help='Number of epochs required for model training')
-
-    parser.add_argument('--learning_rate', type=float, default=0.001,
-                        help='Learning rate of selected optimizer (Adam)')
-
+                        help='Loss function')
+    parser.add_argument('--noise_level', type=float,
+                        default=0.5, help='Noise level for training')
+    parser.add_argument('--batch_size', type=int,
+                        default=32, help='Batch size')
+    parser.add_argument('--epochs', type=int, default=5,
+                        help='Number of training epochs')
+    parser.add_argument('--learning_rate', type=float,
+                        default=0.001, help='Learning rate')
     return parser.parse_args()
 
 
-def train_model(dataset_path: str,
-                dataset_config: RandomPatternConfig,
-                architecture: DiceArchitecture,
-                loss_function: DiceLoss,
-                noise_level: float,
-                num_epochs: int = 20,
-                batch_size: int = 32,
-                learning_rate: float = 0.001):
-
+def prepare_data(dataset_path, split_ratios=(0.75, 0.1, 0.15)):
     data = torch.load(dataset_path, weights_only=False)
-    dataloader = DataLoader(data, batch_size=batch_size)
+    train_size = int(split_ratios[0] * len(data))
+    val_size = int(split_ratios[1] * len(data))
+    test_size = len(data) - train_size - val_size
 
-    model = createDiceModel(architecture).to(device)
-    criterion = createDiceLoss(loss_function, config=dataset_config)
-    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+    train_data, val_data, test_data = random_split(
+        data, [train_size, val_size, test_size])
 
-    progress = Progress(
-        "[progress.description]{task.description}",
-        BarColumn(),
-        TaskProgressColumn(),
-        TextColumn(
-            "[cyan]Epoch: {task.fields[epoch]}/{task.fields[total_epochs]}"),
-        TextColumn(
-            "[green]Entries: {task.fields[entries_used]}/{task.fields[total_entries]}"),
-        TextColumn("[red]Loss: {task.fields[loss]:.4f}"),
-        TimeElapsedColumn(),
-    )
+    return train_data, val_data, test_data
 
-    # Training loop
+
+def train(epochs, dataset_config, dataloaders, model, criterion, optimizer, noise_level):
+    train_loader, val_loader, test_loader = dataloaders
+
     with Live(progress, refresh_per_second=10):
-        task_id = progress.add_task(
-            "[cyan]Epoch progress...",
-            total=len(data),
-            total_entries=len(data),
-            entries_used=0,
-            total_epochs=num_epochs,
-            epoch=0,
-            loss=1.0
-        )
+        for epoch in range(epochs):
+            task_id = progress.add_task(
+                f"[cyan]Epoch {epoch + 1}/{epochs} -",
+                total=len(train_loader),
+                total_entries=len(train_loader)*train_loader.batch_size,
+                entries_used=0,
+                loss=1.0,
+                validation_loss=1.0,
+            )
 
-        for epoch in range(num_epochs):
-            progress.update(task_id, epoch=epoch)
+            def add_channel_dimension(pattern_tensor):
+                return pattern_tensor.unsqueeze(1)
 
-            for i, pattern_tensor in enumerate(dataloader, start=1):
-                pattern_tensor = pattern_tensor.to(device)
+            def add_noise(pattern_tensor):
+                noise_tensor = torch.randn_like(pattern_tensor)
+                return pattern_tensor + noise_tensor * noise_level
 
-                # required by model's - multi-channel architecture
-                pattern_tensor = pattern_tensor.unsqueeze(1)
-
-                noise_tensor = torch.randn_like(pattern_tensor).to(device)
-                noisy_pattern_tensor = pattern_tensor + noise_tensor * noise_level
-
+            # Training Loop
+            model.train()
+            for i, pattern_tensor in enumerate(train_loader, start=1):
+                pattern_tensor = add_channel_dimension(pattern_tensor)
                 optimizer.zero_grad()
-                outputs = model(noisy_pattern_tensor)
+                outputs = model(add_noise(pattern_tensor))
                 loss = criterion(outputs, pattern_tensor)
-
                 loss.backward()
                 optimizer.step()
+                progress.update(task_id, entries_used=i *
+                                train_loader.batch_size, loss=loss)
 
-                # Update progress
-                progress.update(
-                    task_id,
-                    advance=1,
-                    loss=loss.item(),
-                    entries_used=i
-                )
+            # Validation Loop
+            model.eval()
+            validation_loss = 0
+            with torch.no_grad():
+                for pattern_tensor in val_loader:
+                    pattern_tensor = add_channel_dimension(pattern_tensor)
+                    outputs = model(add_noise(pattern_tensor))
+                    validation_loss += criterion(outputs,
+                                                 pattern_tensor) / len(val_loader)
+                    progress.update(task_id, validation_loss=validation_loss)
 
-    return model
+            progress.stop_task(task_id)  # Stop timer for the current epoch
+
+    model.eval()
+    accuracy = 0
+    with torch.no_grad():
+        for pattern_tensor in test_loader:
+            pattern_tensor = add_channel_dimension(pattern_tensor)
+            outputs = model(add_noise(pattern_tensor))
+            outputs[outputs < 0.5] = 0
+            outputs[outputs >= 0.5] = 1
+            outputs = outputs.squeeze(1)
+
+            for output in outputs:
+                if Pattern.tensor_valid_polyphony_requirements(
+                        output, dataset_config.max_polyphony, dataset_config.max_num_events_with_full_polyphony):
+                    accuracy += 1 / (test_loader.batch_size * len(test_loader))
+    return accuracy
 
 
 if __name__ == "__main__":
+    console.print("[bold cyan]DICE Model Training")
     args = parse_args()
 
-    # Compose Import Paths
-    dataset_preset_path = os.path.join(
-        "..", "datasets", "presets", args.preset + ".json")
+    workspace_folder = os.path.abspath(
+        os.path.join(os.path.dirname(__file__), "..", ".."))
 
+    dist_folder = os.path.join(workspace_folder, "dist")
+
+    presets_folder = os.path.join(workspace_folder, "datasets", "presets")
+    datasets_folder = os.path.join(dist_folder, "datasets")
+    models_folder = os.path.join(dist_folder, "models")
+
+    os.makedirs(models_folder, exist_ok=True)
     compiled_dataset_path = os.path.join(
         "..", "dist", "datasets", args.preset + ".pt")
 
-    # Set Distribution Folder
-    script_dir = os.path.dirname(os.path.abspath(__file__))
-    workspace_folder = os.path.abspath(os.path.join(script_dir, "..", ".."))
+    dataset_config_path = os.path.join(presets_folder,  args.preset + ".json")
+    dataset_config = RandomPatternConfig.from_json(dataset_config_path)
 
-    dist_folder = os.path.join(workspace_folder, "dist", "models")
-    os.makedirs(dist_folder, exist_ok=True)
+    compiled_dataset_path = os.path.join(datasets_folder, args.preset + ".pt")
+    train_data, val_data, test_data = prepare_data(compiled_dataset_path)
 
-    # Train Model and Store Result
-    model = train_model(
-        dataset_path=compiled_dataset_path,
-        dataset_config=RandomPatternConfig.from_json(dataset_preset_path),
-        architecture=args.architecture,
-        loss_function=args.loss,
-        noise_level=args.noise_level,
-        batch_size=args.batch_size,
-        learning_rate=args.learning_rate)
-
-    # Store Model and Config data
-    destination_path = os.path.join(
-        dist_folder, args.preset + "-" + args.architecture + "-" + args.loss
+    model = createDiceModel(args.architecture).to(device)
+    accuracy = train(
+        epochs=args.epochs,
+        dataset_config=dataset_config,
+        dataloaders=[
+            DataLoader(train_data, batch_size=args.batch_size, shuffle=True),
+            DataLoader(val_data, batch_size=args.batch_size, shuffle=False),
+            DataLoader(test_data, batch_size=args.batch_size, shuffle=False)
+        ],
+        model=model,
+        criterion=createDiceLoss(args.loss, config=dataset_config),
+        optimizer=optim.Adam(model.parameters(), lr=args.learning_rate),
+        noise_level=args.noise_level
     )
 
-    torch.save(model.state_dict(), destination_path + ".pth")
+    console.print(Text(f"Final Accuracy: {accuracy:.2f}", style="bold green"))
 
+    model_filename = f"{args.preset}-{args.architecture}-{args.loss}"
+    destination_path = os.path.join(models_folder, model_filename)
+
+    torch.save(model.state_dict(), destination_path + ".pth")
     with open(destination_path + ".json", "w") as outfile:
-        outfile.write(json.dumps(vars(args)))
+        json.dump(vars(args), outfile)
+
+    console.print(Text(f"Saved at destination {
+                  destination_path + ".pth"}", style="yellow"))
