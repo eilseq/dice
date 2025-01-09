72898b30ea5245a2dc804f4bfdaf79ea5017e4a4
eilseq <tools@eilseq.com>
2025-01-09 18:30:08 +0100

    add inference tools
    - random matrix handled internally
    - refactoring to reduce scope
    - set additional attributes

--- app/src/lib.rs
+++ app/src/lib.rs
@@ -3,9 +3,8 @@ use median::{
     attr::{AttrBuilder, AttrType},
     builder::MaxWrappedBuilder,
     class::Class,
-    max_sys::t_atom_long,
     num::Float64,
-    object::MaxObj,
+    num::Int64,
     outlet::OutList,
     post,
     wrapper::{attr_get_tramp, attr_set_tramp, MaxObjWrapped, MaxObjWrapper},
@@ -16,7 +15,8 @@ use maplit::hashmap;
 use std::borrow::Cow;
 use wonnx::{utils::InputTensor, utils::OutputTensor, Session};
 
-mod utils;
+mod macros;
+mod matrix;
 
 const ONNX_MODEL: &[u8] =
     include_bytes!("../../dist/models/default-att_unet-mse_poly_penalty.onnx");
@@ -27,6 +27,8 @@ median::external! {
     #[name="dice"]
     pub struct MaxExtern {
         threshold: Float64,
+        noise_level: Float64,
+        seed: Int64,
         list_out: OutList, // Add an outlet field
         session: Session,
     }
@@ -42,6 +44,8 @@ median::external! {
 
             Self {
                 threshold: Float64::new(0.5),
+                noise_level: Float64::new(0.2),
+                seed: Int64::new(0),
                 list_out: builder.add_list_outlet_with_assist("list outlet"), // Initialize outlet
                 session: session.unwrap(),
             }
@@ -60,90 +64,90 @@ median::external! {
                 .unwrap(),
             )
             .expect("failed to add attribute");
+
+            c.add_attribute(
+                AttrBuilder::new_accessors(
+                    "noiseLevel",
+                    AttrType::Float64,
+                    Self::noise_level_tramp,
+                    Self::set_noise_level_tramp,
+                )
+                .build()
+                .unwrap(),
+            )
+            .expect("failed to add attribute");
+
+            c.add_attribute(
+                AttrBuilder::new_accessors(
+                    "seed",
+                    AttrType::Int64,
+                    Self::seed_tramp,
+                    Self::set_seed_tramp,
+                )
+                .build()
+                .unwrap(),
+            )
+            .expect("failed to add attribute");
         }
     }
 
     // Implement additional methods for your object
     impl MaxExtern {
-        // Create a "bang" method and automatically register it
-        #[bang]
-        pub fn bang(&self) {
-            let i = median::inlet::Proxy::get_inlet(self.max_obj());
-            median::object_post!(self.max_obj(), "bang inlet {}", i);
-        }
-
-        // Create an "int" method and automatically register it
-        #[int]
-        pub fn int(&self, v: t_atom_long) {
-            let i = median::inlet::Proxy::get_inlet(self.max_obj());
-            post!("int {} inlet {}", v, i);
-        }
-
-        // Float attribute getter trampoline
         #[attr_get_tramp]
         pub fn threshold(&self) -> f64 {
             self.threshold.get()
         }
 
-        // Float attribute setter trampoline
         #[attr_set_tramp]
         pub fn set_threshold(&self, v: f64) {
             self.threshold.set(v);
         }
 
+        #[attr_get_tramp]
+        pub fn noise_level(&self) -> f64 {
+            self.noise_level.get()
+        }
+
+        #[attr_set_tramp]
+        pub fn set_noise_level(&self, v: f64) {
+            self.noise_level.set(v);
+        }
+
+        #[attr_get_tramp]
+        pub fn seed(&self) -> isize {
+            self.seed.get()
+        }
+
+        #[attr_set_tramp]
+        pub fn set_seed(&self, v: isize) {
+            self.seed.set(v);
+        }
+
         // List method to process and forward the received list to the outlet
         #[list]
         pub fn list(&self, atoms: &[Atom]) {
-            let coo_input: Vec<usize> = atoms.iter().map(|atom| { atom.get_int() as usize }).collect();
-            let flat_input = match utils::binary_matrix::coo_to_flat(coo_input, 16, 16) {
-                Ok(matrix_flat) => matrix_flat.iter().map(|&x| x as f32).collect(),
-                Err(err) => {
-                    post!("Dice Error: {}", err);
-                    return;
-                }
-            };
+            let coo_input = atoms.iter().map(|atom| { atom.get_int() as usize }).collect();
+            let flat_input = handle_result!(matrix::coo_to_flat(coo_input, 16, 16));
+            let flat_flipped = handle_result!(matrix::flat_horizontal_flip(flat_input, 16, 16));
 
+            let noisy_flat_input = matrix::apply_noise(flat_flipped, self.noise_level.get() as f32, self.seed.get() as u64);
             let input_map = hashmap! {
-                "input".to_string() => InputTensor::F32(Cow::Owned(flat_input)),
+                "input".to_string() => InputTensor::F32(Cow::Owned(noisy_flat_input)),
             };
 
             block_on(async {
-                let output_map = match self.session.run(&input_map).await {
-                    Ok(output_map) => output_map,
-                    Err(err) => {
-                        post!("Dice Error: {}", err);
-                        return;
-                    }
-                };
-
-                let output_tensor = match output_map.get("output") {
-                    Some(tensor) => tensor,
-                    None => {
-                        post!("Missing output tensor");
-                        return;
-                    }
-                };
-
+                let output_map = handle_result!(self.session.run(&input_map).await);
+                let output_tensor = handle_option!(output_map.get("output"), "Missing ONNX output tensor");
 
                 match output_tensor {
                     OutputTensor::F32(tensor) => {
-                        let flat_output: Vec<usize> = tensor.to_vec().iter()
-                                        .map(|&x| if x > (self.threshold.get() as f32) { 1 } else { 0 })
-                                        .collect();
-
-                        let coo_output = match utils::binary_matrix::flat_to_coo(flat_output, 16, 16) {
-                            Ok(coo_output) => coo_output,
-                            Err(err) => {
-                                post!("Dice Error: {}", err);
-                                return;
-                            }
-                        };
-
-                        let atom_outputs:Vec<Atom> = coo_output.iter()
+                        let flat_output = matrix::apply_threshold(tensor.to_vec(), self.threshold.get() as f32);
+                        let flat_flipped = handle_result!(matrix::flat_horizontal_flip(flat_output, 16, 16));
+                        let coo_output = handle_result!(matrix::flat_to_coo(flat_flipped, 16, 16));
+                        let atom_outputs = coo_output.iter()
                                         .map(|&x| x as isize)
                                         .map(Atom::from)
                                         .collect::<Vec<Atom>>();
-
                         let _ = self.list_out.send(&atom_outputs);
                     }
                     _ => {
