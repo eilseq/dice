199b4bd91d9e8f54bc299c2972a10f525c88cf24
eilseq <tools@eilseq.com>
2025-01-11 23:44:42 +0100

    update build.rs
    - removes rust-analyzer error
    - forwards compatible path according to file structue
    - uses CARGO_MANIFEST_DIR

--- app/build.rs
+++ app/build.rs
@@ -3,14 +3,17 @@ use std::fs;
 use std::path::Path;
 
 fn main() {
+    // Get the project path from the `CARGO_MANIFEST_DIR` environment variable
+    let project_path =
+        env::var("CARGO_MANIFEST_DIR").expect("CARGO_MANIFEST_DIR is always set by Cargo");
+
     // Default to "target/debug/default_model.onnx" if ONNX_MODEL_PATH is not set
     let model_path = env::var("ONNX_MODEL_PATH")
-        .unwrap_or_else(|_| "target/debug/default_model.onnx".to_string());
+        .unwrap_or_else(|_| project_path + "/target/debug/default_model.onnx");
 
     // Create the dummy file if using the default path and it doesn't already exist
     if model_path == "target/debug/default_model.onnx" {
         let path = Path::new(&model_path);
-
         if !path.exists() {
             fs::write(path, "dummy model content").expect("Failed to create dummy ONNX model");
         }
